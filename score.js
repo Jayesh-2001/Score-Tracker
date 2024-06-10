442fb84fdd9dca835fea0c21669cd9d9e6269402
Jayesh-2001 <jayeshborole2002@gmail.com>
2024-06-10 10:57:54 +0530

    commit 2

--- score.js
+++ score.js
@@ -13,29 +13,7 @@ const winningScoreSelect = document.querySelector('#playto')
 let winningScore = 5;
 let isGameOver = false;
 
-function updateScores(player, opponent) {
-  if (!isGameOver) {
-    player.score += 1;
-    if (player.score === winningScore) {
-      isGameOver = true;
-      player.display.classList.add('has-text-success');
-      opponent.display.classList.add('has-text-danger');
-      player.button.disabled = true;
-      opponent.button.disabled = true;
-    }
-    player.display.textContent = player.score;
-  }
-}
 
-p1.button.addEventListener('click', function () {
-  updateScores(p1, p2)
-})
-
-
-
-p2.button.addEventListener('click', function () {
-  updateScores(p2, p1)
-})
 
 winningScoreSelect.addEventListener('change', function () {
   winningScore = parseInt(this.value);
